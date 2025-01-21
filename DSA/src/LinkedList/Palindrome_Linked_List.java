8ffa96a4f6bd644f25c1a0f451ab0f31668954cd
shivam bhargava <shvmleo@gmal.com>
2025-01-22 01:37:25 +0530

    linkedlist palindrome

---
+++ DSA/src/LinkedList/Palindrome_Linked_List.java
@@ -0,0 +1,45 @@
+package LinkedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class Palindrome_Linked_List {
+    public static boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null)
+            return true;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode head2 = reverseLinkedlist(slow.next);
+        slow.next = null;
+        while (head2 != null) {
+            if (head2.val != head.val)  return false;
+             else {
+                head = head.next;
+                head2 = head2.next;
+            }
+        }
+        return true;
+    }
+
+    public static ListNode reverseLinkedlist(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+}
